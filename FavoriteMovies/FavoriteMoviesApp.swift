72bbf45c35a7338beffa38f29d9432ac11be3848
David Li <dtli@lcs.on.ca>
2023-05-28 16:08:57 -0400

    Initial Commit

---
+++ FavoriteMovies/FavoriteMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteMoviesApp.swift
+//  FavoriteMovies
+//
+//  Created by David Li on 2023-05-28.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
